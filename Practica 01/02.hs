a63d0fe5dda5549e007044719fbe0ccf485bc546
liocutro.cs <lionel.cutro@gmail.com>
2023-11-27 20:02:57 -0300

    Update 02.hs

--- Practica 01/02.hs
+++ Practica 01/02.hs
@@ -12,6 +12,12 @@ sumInt x = x + 1                    -- esta es la funcion que eligo tomar para d
 myInt :: ( Int -> Int ) -> Int
 myInt f = f 4                       -- tomo sumInt y un entero x y devuelve un entero
 
+restInt :: ( Int -> Int )
+restInt x = x - 2
+
+myOperation :: ( Int -> Int ) -> Int
+myOperation f = f 5
+
 -- B ------
 
 myInte :: Integer -> ( Integer -> Integer )
