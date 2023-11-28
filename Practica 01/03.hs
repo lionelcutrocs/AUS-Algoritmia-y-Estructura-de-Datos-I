eead9bb82e88870ae947328a9bf30405fae805e9
liocutro.cs <lionel.cutro@gmail.com>
2023-11-28 17:40:06 -0300

    Update 03.hs
    ejercicio 0A terminado - haskell

--- Practica 01/03.hs
+++ Practica 01/03.hs
@@ -7,3 +7,11 @@
 
 -- f x = let (y,z) = (x,x) in y
 
+f :: a -> a
+f x = let (y, z) = (x, x) in y
+
+-- esta funcion lo que hace es devolver el primer elemento de la lista (x,x)
+-- entonces se puede re definir como
+
+a :: a -> a
+a x = x
