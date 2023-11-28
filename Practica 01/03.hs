a5a947543da340d3887650f726e4ba6d88ba96da
liocutro.cs <lionel.cutro@gmail.com>
2023-11-28 18:10:25 -0300

    Update 03.hs
    ejericio 0C terminado - haskell

--- Practica 01/03.hs
+++ Practica 01/03.hs
@@ -34,3 +34,15 @@ ifGreater (x, y)
 -- C ------
 
 -- f (x,y) = let z = x + y in g (z,y) where g (a,b) = a − b
+
+b :: (Int, Int) -> Int
+b (x, y) = let z = x + y in g (z, y) where g (a, b) = a - b
+
+-- esta funcion toma una tupla, lo cual realiza en un simple modo la resta de x - y
+-- en el momento que se aplica g (z, y) se esta sustituyengo a -> z y b -> y
+-- por lo tanto where g (z, y) = z - y --> es igual que decir que; x - y ya que z = x
+
+-- entonces la puedo redefinir como
+
+c :: (Int, Int) -> Int
+c (x, y) = x - y
